b5f6ae3802ae499b56d92132cef45074f2b269c9
JamesHopbourn <jameshopbourn@gmail.com>
2020-09-13 12:43:02 +0800

    ✨ delete build

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@
 # ====================================================
 
 find . -maxdepth 1 -type f -not -iname "*.*" -delete
+find ./build -maxdepth 1 -type f -not -iname "*.*" -delete
 
 echo -e "Start compiling...\n"
 for file in $(find . -maxdepth 1 -name "$1*.c"|sort -g|xargs basename)
