e61faa175e0d116802c0ebdb89dfdf101f7d2d98
JamesHopbourn <jameshopbourn@gmail.com>
2020-09-13 12:35:02 +0800

    🎨 SE04清屏输出文本 printf

--- SE04清屏输出文本.c
+++ SE04清屏输出文本.c
@@ -4,12 +4,15 @@
 int main(int argc, char const *argv[]) {
   int i;
   system("clear");
-  printf("%*s 欢迎使用学生信息管理系统 v1.0 by:James Hopbourn \n\n", 30, "");
+  printf("%*s \033[36m欢迎使用学生信息管理系统 by:James Hopbourn\033[0m \n", 30,
+         "");
+  printf("\n");
   printf("%*s 1. 添加学生信息 \n", 30, "");
   printf("%*s 2. 修改学生信息 \n", 30, "");
   printf("%*s 3. 删除学生信息 \n", 30, "");
   printf("%*s 4. 查询学生信息 \n", 30, "");
-  printf("%*s 5. 学生成绩导入 \n\n", 30, "");
+  printf("%*s 5. 学生成绩导入 \n", 30, "");
+  printf("\n");
   printf("%*s 请输入数字选择功能：", 30, "");
   scanf("%d", &i);
   system("clear");
