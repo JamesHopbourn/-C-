5813949aaf5c114ccffe9fce1b2db63e0b01fe5c
JamesHopbourn <jameshopbourn@gmail.com>
2020-09-14 12:07:32 +0800

    ✨ 4T22鸡兔同笼问题

--- 4T22鸡兔同笼问题.c
+++ 4T22鸡兔同笼问题.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-  int Animals = 88;
-  int SumFoots = 0;
-  int RabbitFoots = 4;
-  int ChickenFoots = 2;
-  do {
-    SumFoots = RabbitFoots * Animals;
-    Animals = Animals - 1;
-    printf("%d\n", Animals);
-  } while (SumFoots == 244);
+  int heads = 88;
+  int foots = 224;
+  int rabbit, chicken;
+  rabbit = (foots / 2) - heads;
+  chicken = heads - rabbit;
+  printf("鸡的数量：%d   兔子的数量：%d\n", chicken, rabbit);
   return 0;
 }
