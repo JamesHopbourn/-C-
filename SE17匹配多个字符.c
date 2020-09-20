695f9b1b2ea0c63364a88a0ad9b6b7cdb96226ba
JamesHopbourn <jameshopbourn@gmail.com>
2020-09-20 11:36:53 +0800

    ✨ SE17匹配多个字符

---
+++ SE17匹配多个字符.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  char answer = 0;
+
+  printf("Enter Y or N: ");
+  scanf(" %c", &answer);
+
+  switch (answer) {
+  case 'y':
+  case 'Y':
+    printf("You responded in the affirmative.");
+    break;
+
+  case 'n':
+  case 'N':
+    printf("You responded in the negative.");
+    break;
+
+  default:
+    printf("You did not respond correctly...");
+    break;
+  }
+  return 0;
+}
+
+// http://www.java2s.com/Tutorial/C/0120__Statement/Twocasesoneaction.htm
