36732df3b87d886708b91dc2c607a95f9e52df6c
JamesHopbourn <jameshopbourn@gmail.com>
2020-10-21 06:05:19 +0800

    🎨 .clang-format

--- 703三个整数求最大值.c
+++ 703三个整数求最大值.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int mymax(int a, int b, int c);
+int maxNumber(int a, int b, int c);
 
 int main(int argc, char const *argv[]) {
   int a, b, c;
   printf("请输入三个数字，以空格间隔：");
   scanf("%d %d %d", &a, &b, &c);
-  printf("最大值为：%d\n", mymax(a, b, c));
+  printf("最大值为：%d\n", maxNumber(a, b, c));
   return 0;
 }
 
-int mymax(int a, int b, int c) { return ((a > b) ? (a > c) ? a : b : c); }
+int maxNumber(int a, int b, int c) {
+  return ((a > b) ? (a > c) ? a : b : c);
+}
