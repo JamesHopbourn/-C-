1992bb3274da466f6a6f2e6748074106d2e9ec7d
JamesHopbourn <jameshopbourn@gmail.com>
2020-10-27 23:52:35 +0800

    ✨ SE29字符串的输入.c

---
+++ SE29字符串的输入.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+  int start,length;
+  char str[100];
+  printf("请输入字符：");
+  gets(str);
+  length = (int)strlen(str);
+  printf("%s\n", str);
+  printf("%d\n", length);
+}
