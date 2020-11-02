a0e8536d1537746f6f2e59be91cf6768d93d2dcc
JamesHopbourn <jameshopbourn@gmail.com>
2020-11-02 12:31:04 +0800

    🚚 SE25字符串含空格

--- SE32定义简单函数.c
+++ SE32定义简单函数.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-#define min(a,b) ((a)<(b)?(a):(b))
-#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
-int main(){
-   int a, b, c, max_number,min_number;
-   scanf("%d %d %d",&a,&b,&c);
-   min_number = min(min(a, b), c);
-   max_number = max(max(a, b), c);
-   printf("LOWEST %d\n", min_number);
-   printf("MIDDLE %d\n", a+b+c-max_number-min_number);
-   printf("HIGHEST %d\n", max_number);  
+int main() {
+  int a, b, c, max_number, min_number;
+  scanf("%d %d %d", &a, &b, &c);
+  min_number = min(min(a, b), c);
+  max_number = max(max(a, b), c);
+  printf("LOWEST %d\n", min_number);
+  printf("MIDDLE %d\n", a + b + c - max_number - min_number);
+  printf("HIGHEST %d\n", max_number);
 
-   getchar(); 
+  getchar();
 }
